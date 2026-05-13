046ae41e9571a0f8cf58575d29db61ff62281f63
marcel615 <alswo000615@gmail.com>
2026-05-13 18:18:22 +0900

    feat: Channel 클래스 구현
    - Channel 클래스: 필드, 생성자, getter, setter, 기본 메서드(User, Message 추가 및 제거) 구현

--- src/main/java/com/sprint/mission/discodeit/entity/Channel.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Channel.java
@@ -1,5 +1,7 @@
 package com.sprint.mission.discodeit.entity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class Channel {
@@ -10,7 +12,78 @@ public class Channel {
     private Long updatedAt;
 
     //필드
+    private String name;
+    private List<User> users;
+    private List<Message> messages;
 
+    //ctor
+    public Channel(String name) {
+        id = UUID.randomUUID();
+        createdAt = System.currentTimeMillis();
+        updatedAt = System.currentTimeMillis();
 
+        this.name = name;
+        users = new ArrayList<>();
+        messages = new ArrayList<>();
+    }
+
+    //getter
+    public UUID getId() {
+        return id;
+    }
+
+    public Long getCreatedAt() {
+        return createdAt;
+    }
+
+    public Long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    //setter
+    public void setName(String name) {
+        if (name == null || name.isBlank())
+            return;
+
+        this.name = name;
+    }
+
+    //method
+    public void addUser(User user) {
+        if (users.contains(user) || user == null)
+            return;
+
+        users.add(user);
+    }
+    public void removeUser(User user) {
+        if (!users.contains(user) || user == null)
+            return;
+
+        users.remove(user);
+    }
+    public void addMessage(Message message) {
+        if (messages.contains(message) || message == null)
+            return;
+
+        messages.add(message);
+    }
+    public void removeMessage(Message message) {
+        if (!messages.contains(message) || message == null)
+            return;
+
+        messages.remove(message);
+    }
 
 }
