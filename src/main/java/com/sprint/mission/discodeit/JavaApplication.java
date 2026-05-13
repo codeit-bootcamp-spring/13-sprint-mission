df200979e1750b09ed2202ade78475e5a2655271
marcel615 <alswo000615@gmail.com>
2026-05-13 16:40:19 +0900

    feat: JavaApplication.java 생성

---
+++ src/main/java/com/sprint/mission/discodeit/JavaApplication.java
@@ -0,0 +1,11 @@
+package com.sprint.mission.discodeit;
+
+public class JavaApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is Discodeit Application!");
+
+    }
+
+}
