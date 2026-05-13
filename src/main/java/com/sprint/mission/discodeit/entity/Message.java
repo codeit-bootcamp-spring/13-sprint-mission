f1a939ed7386d0b35b38166806f8989cbb953b8d
marcel615 <alswo000615@gmail.com>
2026-05-13 18:52:08 +0900

    feat: Message 클래스 구현
    - Message 클래스: 필드, 생성자, getter, 기본 메서드(Message 수정) 구현

--- src/main/java/com/sprint/mission/discodeit/entity/Message.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.entity;
 
+import java.util.List;
 import java.util.UUID;
 
 public class Message {
@@ -10,7 +11,51 @@ public class Message {
     private Long updatedAt;
 
     //필드
+    private String message;
+    private User user;
+    private Channel channel;
 
+    //ctor
+    public Message(String message, User user, Channel channel) {
+        id = UUID.randomUUID();
+        createdAt = System.currentTimeMillis();
+        updatedAt = System.currentTimeMillis();
 
+        this.message = message;
+        this.user = user;
+        this.channel = channel;
+    }
+
+    //getter
+    public UUID getId() {
+        return id;
+    }
+
+    public Long getCreatedAt() {
+        return createdAt;
+    }
+
+    public Long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Channel getChannel() {
+        return channel;
+    }
+
+    //method
+    public void updateMessage(Message message) {
+        if (message.message == null || message.message.isBlank())
+            return;
+        this.message = message.message;
+    }
 
 }
