7e59cf8085985ec580f4560ed974ff9501f17445
marcel615 <alswo000615@gmail.com>
2026-05-14 10:10:34 +0900

    feat(service): UserService, ChannelService, MessageService 클래스 생성

---
+++ src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -0,0 +1,11 @@
+package com.sprint.mission.discodeit.service;
+
+public interface MessageService {
+
+    /*
+    - user 관련
+    - 해당 메세지의 작성자 읽기(출력) 메서드
+- channel 관련
+    - 해당 메세지가 작성된 채널 정보 읽기(출력) 메서드
+     */
+}
