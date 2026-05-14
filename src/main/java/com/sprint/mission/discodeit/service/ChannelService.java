63f7944fcd88b18df9cbb7be7b3d9c262035ca93
marcel615 <alswo000615@gmail.com>
2026-05-14 16:32:06 +0900

    refactor(service): ChannelService 인터페이스 수정
    - editChannelName메서드 파라미터 추가

--- src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
@@ -8,7 +8,7 @@ public interface ChannelService {
 
     Channel createChannel(String name, User channelHost);
     void printChannelInfo(Channel channel);
-    void editChannelName(Channel channel, String newName);
+    void editChannelName(Channel channel, User user, String newName);
     Channel deleteChannel(Channel channel, User user);
 
     void addUserToChannel(Channel channel, User user);
