5c81df3d67f6f93b41817979ad5fd34a54d7d5e5
marcel615 <alswo000615@gmail.com>
2026-05-14 16:10:51 +0900

    feat(jcf): JCFMessageService 클래스 구현
    - MessageService 인터페이스 구현

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
@@ -1,4 +1,60 @@
 package com.sprint.mission.discodeit.service.jcf;
 
-public class JCFMessageService {
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.MessageService;
+
+public class JCFMessageService implements MessageService {
+
+
+    // interface
+    @Override
+    public Message createMessage(User user, Channel channel, String message) {
+        if (message == null || message.isBlank()) throw new RuntimeException("메세지는 공백일 수 없습니다.");
+        if (!user.getChannels().contains(channel)) throw new RuntimeException("해당 유저는 이 채널에 존재하지 않습니다.");
+        if (message == null || user == null) throw new RuntimeException("메세지, 유저는 null이면 안됩니다.");
+
+        return new Message(message, user, channel);
+    }
+
+    @Override
+    public void printMessage(Message message) {
+        if (message == null) throw new RuntimeException("메세지는 null이면 안됩니다.");
+
+        System.out.println(message);
+    }
+
+    @Override
+    public void editMessage(Message message, User user, String newMessage) {
+        if (newMessage == null || newMessage.isBlank()) throw new RuntimeException("새 메세지는 공백일 수 없습니다.");
+        if (user != message.getUser()) throw new RuntimeException("해당 메세지 작성자가 아니므로 수정 불가.");
+        if (message == null || user == null) throw new RuntimeException("메세지, 유저는 null이면 안됩니다.");
+
+        message.updateMessage(newMessage);
+    }
+
+    @Override
+    public Message deleteMessage(Message message, User user) {
+        if (user != message.getUser()) throw new RuntimeException("해당 메세지 작성자가 아니므로 삭제 불가.");
+        if (message == null || user == null) throw new RuntimeException("메세지, 유저는 null이면 안됩니다.");
+
+        message.getUser().removeMessage(message);
+        message.getChannel().removeMessage(message);
+        return null;
+    }
+
+    @Override
+    public void printWriter(Message message) {
+        if (message == null) throw new RuntimeException("메세지는 null이면 안됩니다.");
+
+        System.out.println("Writer: " + message.getUser().getName());
+    }
+
+    @Override
+    public void printChannel(Message message) {
+        if (message == null) throw new RuntimeException("메세지는 null이면 안됩니다.");
+
+        System.out.println("Wrote Channel Name: " + message.getChannel().getName());
+    }
 }
