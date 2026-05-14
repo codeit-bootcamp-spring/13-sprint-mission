3bc1b5d6084a6c1417fe63e505ecd245e412528a
jangjunseo518-collab <wkdwnstj00@naver.com>
2026-05-14 14:50:07 +0900

    커밋 2: 인터페이스 -인터페이스 제작 메세지, 유저, 채널
    추상메서드 구현.
    필드를 활용하여 생성, 조회 수정 삭제를
    하는 추상 메서드 구현

---
+++ src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -0,0 +1,21 @@
+package com.sprint.mission.discodeit.service;
+
+import com.sprint.mission.discodeit.entity.Message;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface MessageService {
+    //필드를 활용해 생성, 조회, 수정, 삭제하는 메소드를 구현하세요.
+    void create(Message message);//생성
+
+    Message read(UUID id);//조회
+
+    List<Message> readAll();
+
+    void update(UUID id, Message message);
+
+    void delete(UUID id);
+
+
+}
