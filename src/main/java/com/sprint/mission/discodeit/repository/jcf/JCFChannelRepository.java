7bdf08aaf985b6b4796422974ae8f187cc250a28
seok-dev <kseok.dev@gmail.com>
2026-05-26 14:38:54 +0900

    ChannelRepository 인터페이스 생성
    JCFChannelService 저장 로직 구현

---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFChannelRepository.java
@@ -0,0 +1,32 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+
+
+import java.util.*;
+
+public class JCFChannelRepository implements ChannelRepository {
+
+    private final Map<UUID, Channel> data = new HashMap<>();
+
+    @Override
+    public void save(Channel channel) {
+        data.put(channel.getId(), channel);
+    }
+
+    @Override
+    public Channel findById(UUID channelId) {
+        return data.get(channelId);
+    }
+
+    @Override
+    public List<Channel> findAll() {
+        return new ArrayList<>(data.values());
+    }
+
+    @Override
+    public void deleteById(UUID channelId) {
+        data.remove(channelId);
+    }
+}
