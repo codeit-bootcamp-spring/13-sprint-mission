e4290b282b851eb9cdcec398150e0b26c3f76940
seok-dev <kseok.dev@gmail.com>
2026-05-26 14:40:14 +0900

    MessageRepository 인터페이스 생성
    JCFMessageRepository 저장 로직 구현

---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
@@ -0,0 +1,40 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class JCFMessageRepository implements MessageRepository {
+
+    private final Map<UUID, Message> data = new HashMap<>();
+    @Override
+    public void save(Message message) {
+        data.put(message.getId(), message);
+    }
+
+    @Override
+    public Message findById(UUID messageId) {
+        return data.get(messageId);
+    }
+
+    @Override
+    public List<Message> findAllByChannelId(UUID channelId) {
+        return data.values().stream()
+                .filter(message -> message.getChannelId().equals(channelId))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Message> findAllByUserId(UUID userId) {
+        return data.values().stream()
+                .filter(message -> message.getAuthorId().equals(userId))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        data.remove(id);
+    }
+}
