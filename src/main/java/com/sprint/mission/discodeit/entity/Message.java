28319f265c7a4e4507c6571778f03944dd4858b5
y2ahhh <yeaheunlee1190@gmail.com>
2026-05-28 22:34:49 +0900

    feat: Message CRUD 기능 구현

---
+++ src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -0,0 +1,51 @@
+package com.sprint.mission.discodeit.entity;
+
+import java.util.UUID;
+
+public class Message {
+    private UUID id;
+    private Long createdAt;
+    private Long updatedAt;
+    private String content;
+    private UUID userId;
+    private UUID channelId;
+
+    public Message(UUID userId, UUID channelId, String content) {
+        this.id = UUID.randomUUID();
+        this.createdAt = System.currentTimeMillis();
+        this.updatedAt = createdAt;
+
+        this.userId = userId;
+        this.channelId = channelId;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public UUID getChannelId() {
+        return channelId;
+    }
+
+    public Long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void update(String content) {
+        this.content = content;
+        this.updatedAt = System.currentTimeMillis();
+    }
+}
