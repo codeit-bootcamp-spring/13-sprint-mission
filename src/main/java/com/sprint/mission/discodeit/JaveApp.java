d112c288b8276b60374d96706f5b10b3e876da4a
jjeong98 <jjeong98@outloook.com>
2026-05-31 18:10:27 +0900

    스프린트 2차 적용 사항 (심화요구사항 - 관심사 분리를 통한 의존성 주입) feat - BasicService - BasicService - User/Channel/Mesage 구현체 생성 및 작성
    Test - JCF// File에 대한 테스트 완료

--- src/main/java/com/sprint/mission/discodeit/JaveApp.java
+++ src/main/java/com/sprint/mission/discodeit/JaveApp.java
@@ -1,26 +1,42 @@
 package com.sprint.mission.discodeit;
 
+
+
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.repository.file.FileChannelRepository;
+import com.sprint.mission.discodeit.repository.file.FileMessageRepository;
+import com.sprint.mission.discodeit.repository.file.FileUserRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFChannelRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFMessageRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFUserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
-import com.sprint.mission.discodeit.service.file.FileChannelService;
-import com.sprint.mission.discodeit.service.file.FileMessageService;
-import com.sprint.mission.discodeit.service.file.FileUserService;
-import com.sprint.mission.discodeit.service.jcf.JCFChannelService;
-import com.sprint.mission.discodeit.service.jcf.JCFMessageService;
-import com.sprint.mission.discodeit.service.jcf.JCFUserService;
+import com.sprint.mission.discodeit.service.basic.BasicChannelService;
+import com.sprint.mission.discodeit.service.basic.BasicMessageService;
+import com.sprint.mission.discodeit.service.basic.BasicUserService;
+
 
 import java.util.Collection;
 
 public class JaveApp {
 	public static void main(String[] args) {
-		
-		UserService userService = new FileUserService();
-		ChannelService channelService = new FileChannelService();
-		MessageService messageService = new FileMessageService(userService, channelService);
+
+		UserRepository userRepository = new FileUserRepository(); //new JCFUserRepository();
+		ChannelRepository channelRepository = new FileChannelRepository(); //JCFChannelRepository();
+		MessageRepository messageRepository = new FileMessageRepository(); //JCFMessageRepository();
+
+		UserService userService = new BasicUserService(userRepository);
+		ChannelService channelService = new BasicChannelService(channelRepository);
+		MessageService messageService = new BasicMessageService(messageRepository, userRepository, channelRepository);
+
+
+
 		
 		// 유저 생성
 		User user1 = new User("KJH", "KJH@kjh.com");
---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -0,0 +1,47 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public class BasicUserService implements UserService {
+
+    private final UserRepository userRepository;
+    public BasicUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public void create(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public User findById(UUID id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public Collection<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void update(UUID id, String name, String email) {
+        User user= userRepository.findById(id);
+
+        if (user != null) {
+            user.renew(name, email);
+            userRepository.save(user);
+        }
+    }
+
+    @Override
+    public void delete(UUID id) {
+        userRepository.delete(id);
+    }
+}
