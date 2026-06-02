51646943aa1741fa1a09f78daa1145eb7bb5bf6c
woong <skarn506@gmail.com>
2026-06-02 18:30:25 +0900

    UserService 고도화 - DTO를 활용하여 파라미터 그룹화

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -1,5 +1,8 @@
 package com.sprint.mission.discodeit.service;
 
+import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
+import com.sprint.mission.discodeit.dto.request.UserUpdateRequest;
+import com.sprint.mission.discodeit.dto.response.UserResponse;
 import com.sprint.mission.discodeit.entity.User;
 
 import java.util.List;
@@ -7,10 +10,10 @@ import java.util.UUID;
 
 public interface UserService {
 
-    User create(String username, String email, String password);
-    User findById(UUID userId);
-    List<User> findAll();
-    User update(UUID userId, String newUsername, String newEmail, String newPassword);
+    UserResponse create(UserCreateRequest request);
+    UserResponse findById(UUID userId);
+    List<UserResponse> findAll();
+    UserResponse update(UserUpdateRequest request);
     void delete(UUID userId);
 
 }
