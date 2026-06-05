1d8abfa5c417cfb845495d19ff688d2ce5e0d042
s-j012 <ddrwsy3800@naver.com>
2026-06-05 19:11:00 +0900

    스프린트 미션2 1차 추가 도메인 모델링 (User.java, Channel.java, Message.java, ChannelType.java)클래스 내용추가

--- src/main/java/com/sprint/mission/discodeit/entity/Channel.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Channel.java
@@ -1,34 +1,54 @@
 package com.sprint.mission.discodeit.entity;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.UUID;
 
 // ##채팅방(채널) 정보를 담는 클래스
 public class Channel implements Serializable {
     private final UUID id; //고유번호
-    private final Long createdAt; //만든시각
+    private Long createdAt; //만든시각
     private Long updatedAt;  //바뀐시각
     private String name; //채널이름
+    private String description;
+    private ChannelType type;
 
     // 생성자(id/createdAt 생성자에서 초기화, id/createdAt/updateAt 제외한 필드는 생성자의 파라미터를 통해 초기화
     //생성자: 이름을 받아서 Channel 객체를 만듬
-    public Channel(String name) {
+    public Channel(ChannelType type, String name, String description) {
         this.id = UUID.randomUUID(); //고유 아이디 자동 생성
         this.createdAt = System.currentTimeMillis(); //생성시점 자동 기록
-        this.updatedAt = this.createdAt; //처음엔 생성 시각과 같음
+        this.type = type;
         this.name = name; //생성할 떄 이름만 받음
+        this.description = description;
     }
 
     //각 필드를 반환하는 getter 함수
     //정보 꺼내는 함수도 있어요
     public UUID getId() {return id;}
-    public long getCreatedAt() {return createdAt;}
-    public long getUpdatedAt() {return updatedAt;}
+    public Long getCreatedAt() {return createdAt;}
+    public Long getUpdatedAt() {return updatedAt;}
+
+    public ChannelType getType() {
+        return type;
+    }
+
     public String getName() {return name;}
+    public String getDescription() {return description;}
 
     //이름을 수정하는 update 함수 (수정 시 update도 갱신)
-    public void updateName(String newName) {
-        this.name = newName;
-        this.updatedAt = System.currentTimeMillis();
+    public void updateUser(String newName, String newDescription) {
+        boolean anyValueUpdated = false;
+        if (newName == null || newName.isEmpty()) {
+            this.name = newName;
+            anyValueUpdated = true;
+        }
+        if (newDescription == null || newDescription.isEmpty()) {
+            this.description = newDescription;
+            anyValueUpdated = true;
+        }
+        if (!anyValueUpdated) {
+            this.updatedAt = Instant.now().getEpochSecond();
+        }
     }
 }
--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -1,6 +1,7 @@
 package com.sprint.mission.discodeit.entity;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.UUID;
 
 // ##사용자 정보를 담는 클래스
@@ -11,14 +12,17 @@ public class User implements Serializable {
     private final Long createdAt; //만든시각
     private Long updatedAt;  //바뀐시각
     private String name; //사용자이름
+    private String email;
+    private String password;
 
     // 생성자(id/createdAt 생성자에서 초기화, id/createdAt/updateAt 제외한 필드는 생성자의 파라미터를 통해 초기화)
     // 생성자: 이름을 받아서 User 객체를 만듬.
-    public User(String name) { //만들 때 이름만 받음
+    public User(String name, String email, String password) { //만들 때 이름만 받음
         this.id = UUID.randomUUID(); //고유 번호 자동 생성
         this.createdAt = System.currentTimeMillis(); //지금 시각 (생성 시각 자동기록)
-        this.updatedAt = this.createdAt; //같은 값으로 시작 (처음엔 생성시각과 같음)
         this.name = name; //생성 할때 이름을 받음
+        this.email = email;
+        this.password = password;
     }
 
     //각 필드에 받환하는 getter 함수
@@ -26,14 +30,30 @@ public class User implements Serializable {
     public UUID getId() {
         return id;
     }
-    public long getCreatedAt() {return createdAt;}
-    public long getUpdatedAt() {return updatedAt;}//이름 꺼내는 함수
+    public Long getCreatedAt() {return createdAt;}
+    public Long getUpdatedAt() {return updatedAt;}//이름 꺼내는 함수
     public String getName() {return name;}
+    public String getEmail() {return email;}
+    public String getPassword() {return password;}
 
     //이름을 수정하는 update 함수 (수정 시 update도 갱신)
     //update 함수 : name 값을 바꿔중
-    public void updateName(String newName) { //이름 바꾸는 함수
-        this.name = newName;
-        this.updatedAt = System.currentTimeMillis(); //바꿀 떄마다 시각도 변경
+    public void updateUser(String newUsername, String newEmail, String newPassword) {
+        boolean anyValueUpdated = false;
+        if (newUsername != null && !newUsername.equals(this.name)) {
+            this.name = newUsername;
+            anyValueUpdated = true;
+        }
+        if (newEmail != null && !newEmail.equals(this.email)) {
+            this.email = newEmail;
+            anyValueUpdated = true;
+        }
+        if (newPassword != null && !newPassword.equals(this.password)) {
+            this.password = newPassword;
+            anyValueUpdated = true;
+        }
+        if (anyValueUpdated) {
+            this.updatedAt = Instant.now().getEpochSecond();
+        }
     }
 }
