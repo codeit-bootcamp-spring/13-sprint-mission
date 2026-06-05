ce3c25df843d99e22bf5d07bc4761c15637b33e4
marcel615 <alswo000615@gmail.com>
2026-06-05 10:35:55 +0900

    feat: 스프린트 미션3 #11
    - DTO 생성 및 구현 (BinaryContentCreateRequest)
    - BinaryContentService 인터페이스 구현
    - BinaryContentRepository 인터페이스에 메서드 추가
    - BasicBinaryContentService 구현

--- src/main/java/com/sprint/mission/discodeit/repository/BinaryContentRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/BinaryContentRepository.java
@@ -3,6 +3,7 @@ package com.sprint.mission.discodeit.repository;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,6 +11,8 @@ public interface BinaryContentRepository {
 
     void createBinaryContent(BinaryContent binaryContent);
     Optional<BinaryContent> findBinaryContentByContentPath(String contentPath);
+    Optional<BinaryContent> findBinaryContentById(UUID binaryContentId);
+    List<BinaryContent> findAllBinaryContentByIdIn(List<UUID> binaryContentIds);
     void deleteBinaryContent(UUID id);
 
 }
---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicBinaryContentService.java
@@ -0,0 +1,102 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.repository.BinaryContentRepository;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class BasicBinaryContentService implements BinaryContentService {
+
+    //필드
+    private final BinaryContentRepository binaryContentRepository;
+
+    //interface
+    @Override
+    public BinaryContent createBinaryContent(BinaryContentCreateRequest request) {
+        //입력값 검증 처리하겠습니다
+        validateString(request.contentPath());
+
+        //요청받은 Content경로가 유효한지 검증
+        validateContentPath(request.contentPath());
+
+        //BinaryContent 생성
+        BinaryContent binaryContent = new BinaryContent(request.contentPath());
+        binaryContentRepository.createBinaryContent(binaryContent);
+        log.info("BinaryContent가 생성됨.");
+
+        return binaryContent;
+    }
+
+    @Override
+    public BinaryContent findBinaryContentById(UUID binaryContentId) {
+        //입력값 검증 처리하겠습니다
+        validateUUID(binaryContentId);
+
+        //BinaryContent 검색
+        BinaryContent binaryContentTemp = binaryContentRepository.findBinaryContentById(binaryContentId)
+                .orElseThrow(() -> new RuntimeException("에러: 해당 BinaryContent는 데이터파일에 존재하지 않습니다."));
+
+        return binaryContentTemp;
+    }
+
+    @Override
+    public List<BinaryContent> findAllBinaryContentByIdIn(List<UUID> binaryContentIds) {
+        //입력값 검증 처리하겠습니다
+        for (UUID binaryContentId : binaryContentIds) {
+            validateUUID(binaryContentId);
+        }
+
+        //BinaryContent들 검색
+        List<BinaryContent> binaryContentList = binaryContentRepository.findAllBinaryContentByIdIn(binaryContentIds);
+
+        return binaryContentList;
+    }
+
+    @Override
+    public void deleteBinaryContent(UUID binaryContentId) {
+        //입력값 검증 처리하겠습니다
+        validateUUID(binaryContentId);
+
+        //BinaryContent 검색
+        BinaryContent binaryContentTemp = binaryContentRepository.findBinaryContentById(binaryContentId)
+                .orElseThrow(() -> new RuntimeException("에러: 해당 BinaryContent는 데이터파일에 존재하지 않습니다."));
+
+        binaryContentRepository.deleteBinaryContent(binaryContentId);
+
+        log.info("BinaryContent: {}가 삭제됨.", binaryContentTemp.getId());
+    }
+
+
+    // 들어온 String 필드가 null 혹은 공백인지 검증하는 메서드
+    private void validateString(String str) {
+        if (str == null || str.isBlank()) {
+            throw new IllegalArgumentException("에러: 입력값이 Null 또는 공백입니다.");
+        }
+    }
+
+    // 들어온 UUID 필드가 null인지 검증하는 메서드
+    private void validateUUID(UUID id) {
+        if (id == null) {
+            throw new IllegalArgumentException("에러: 입력값이 Null입니다.");
+        }
+    }
+
+    //요청받은 Content경로가 유효한지 검증하는 메서드
+    private void validateContentPath(String path) {
+        if (!Files.exists(Path.of(path))) {
+            throw new IllegalArgumentException("에러: ContentPath가 유효하지 않습니다.");
+        }
+    }
+}
