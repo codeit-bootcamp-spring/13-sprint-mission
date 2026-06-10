d3ff9253fb2450644d64d51bdfb2fad505eb3964
seok-dev <kseok.dev@gmail.com>
2026-06-10 15:01:29 +0900

    BinaryContent 검증 로직 추가 - 파일 크기 검증 - 파일 타입 검증 ContentType - Enum선언

--- src/main/java/com/sprint/mission/discodeit/dto/binarycontent/BinaryContentCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/binarycontent/BinaryContentCreateRequest.java
@@ -1,5 +1,8 @@
 package com.sprint.mission.discodeit.dto.binarycontent;
 
+import com.sprint.mission.discodeit.entity.ContentType;
+
+import java.util.Arrays;
 import java.util.UUID;
 
 public record BinaryContentCreateRequest(
@@ -10,5 +13,20 @@ public record BinaryContentCreateRequest(
         byte[] bytes
 )
 {
+    private static final long MAX_FILE_SIZE = 5 * 1024 * 1024; //5MB
+
+    public BinaryContentCreateRequest{
+        //파일 크기 검증
+        if (fileSize > MAX_FILE_SIZE) {
+            throw new IllegalArgumentException("파일 크기는 5MB 이하여야 합니다.");
+        }
+
+        //파일 타입검증
+        boolean isAllowed = Arrays.stream(ContentType.values())
+                .anyMatch(ct -> ct.getValue().equals(contentType));
+        if (!isAllowed) {
+            throw new IllegalArgumentException("지원하지 않는 파일 형식입니다.");
+        }
+    }
 
 }
---
+++ src/main/java/com/sprint/mission/discodeit/entity/ContentType.java
@@ -0,0 +1,26 @@
+package com.sprint.mission.discodeit.entity;
+
+public enum ContentType {
+
+    //이미지
+    IMAGE_JPEG("image/jpeg"),
+    IMAGE_PNG("image/png"),
+    IMAGE_GIF("image/gif"),
+
+    //문서
+    APPLICATION_PDF("application/pdf"),
+
+    //텍스트
+    TEXT_PLAIN("text/plain");
+
+    private final String value;
+
+    ContentType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
