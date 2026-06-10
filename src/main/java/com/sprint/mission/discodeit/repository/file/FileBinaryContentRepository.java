df94fb2835ee7c8df40dd7c8637113ccb894ca35
hajung <bover80@naver.com>
2026-06-10 15:48:29 +0900

    UserStatus, ReadStatus, BinaryContent File 구현체 생성

---
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileBinaryContentRepository.java
@@ -0,0 +1,109 @@
+package com.sprint.mission.discodeit.repository.file;
+
+import com.sprint.mission.discodeit.entity.*;
+import com.sprint.mission.discodeit.repository.*;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class FileBinaryContentRepository implements BinaryContentRepository {
+
+    private final List<BinaryContent> binaryContents = new ArrayList<>();
+    private final Path binaryContentPath;
+
+
+    public FileBinaryContentRepository(Path binaryContentPath) {
+        this.binaryContentPath = binaryContentPath;
+        loadFromFile();
+    }
+
+    public FileBinaryContentRepository() {
+        this(Paths.get("data/binaryContents.ser"));
+    }
+
+    @Override
+    public List<BinaryContent> findAllByMessageId(UUID id) {
+        return binaryContents.stream()
+                .filter(content -> content.getMessageId().equals(id))
+                .toList();
+    }
+
+    @Override
+    public BinaryContent findByUserId(UUID id) {
+        return binaryContents.stream()
+                .filter(content -> content.getUserId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public void create(BinaryContent binaryContent) {
+        binaryContents.add(binaryContent);
+        saveToFile();
+    }
+
+    @Override
+    public List<BinaryContent> findAllByIdIn(List<UUID> ids) {
+        return binaryContents.stream()
+                .filter(content -> ids.contains(content.getId()))
+                .toList();
+    }
+
+    @Override
+    public BinaryContent find(UUID id) {
+        return binaryContents.stream()
+                .filter(content -> content.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public void delete(UUID id) {
+        binaryContents.removeIf(binaryContent ->
+                Objects.equals(binaryContent.getId(), id)
+        );
+        saveToFile();
+    }
+
+    private void saveToFile() {
+        try {
+            Path parent = binaryContentPath.getParent();
+
+            if (parent != null) {
+                Files.createDirectories(parent);
+            }
+            try (ObjectOutputStream oos = new ObjectOutputStream(
+                    new BufferedOutputStream(Files.newOutputStream(binaryContentPath)))) {
+
+                oos.writeObject(binaryContents);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("파일 저장 중 오류가 발생했습니다.", e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void loadFromFile() {
+        if (!Files.exists(binaryContentPath)) {
+            return;
+        }
+
+        try(ObjectInputStream ois = new ObjectInputStream
+                (new BufferedInputStream(Files.newInputStream(binaryContentPath)))) {
+
+            List<BinaryContent> loadedBinary = (List<BinaryContent>) ois.readObject();
+            binaryContents.clear();
+            binaryContents.addAll(loadedBinary);
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException("파일을 불러오기 중에 오류가 발생했습니다.", e);
+        }
+    }
+
+
+}
+
+
+
+
---
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileUserStatusRepository.java
@@ -0,0 +1,104 @@
+package com.sprint.mission.discodeit.repository.file;
+
+import com.sprint.mission.discodeit.entity.*;
+import com.sprint.mission.discodeit.repository.*;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class FileUserStatusRepository implements UserStatusRepository {
+
+    private final List<UserStatus> userStatuses = new ArrayList<>();
+    private final Path userStatusPath;
+
+    public FileUserStatusRepository(Path userStatusPath) {
+        this.userStatusPath = userStatusPath;
+        loadFromFile();
+    }
+
+    public FileUserStatusRepository() {
+        this(Paths.get("data/userStatuses.ser"));
+    }
+
+    @Override
+    public UserStatus findByUserId(UUID id) {
+        return userStatuses.stream()
+                .filter(userStatus -> userStatus.getUserId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public void create(UserStatus userStatus) {
+        userStatuses.add(userStatus);
+        saveToFile();
+    }
+
+    @Override
+    public UserStatus find(UUID id) {
+        return userStatuses.stream()
+                .filter(userStatus -> userStatus.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public List<UserStatus> findAll() {
+        return List.copyOf(userStatuses);
+    }
+
+    @Override
+    public void delete(UUID id) {
+        userStatuses.removeIf(userStatus ->
+                Objects.equals(userStatus.getId(), id)
+        );
+        saveToFile();
+    }
+
+    @Override
+    public void update(UserStatus userStatus) {
+        int index = userStatuses.indexOf(find(userStatus.getId()));
+
+        if (index != -1) {
+            userStatuses.set(index, userStatus);
+            saveToFile();
+        }
+    }
+
+    private void saveToFile() {
+        try {
+            Path parent = userStatusPath.getParent();
+
+            if (parent != null) {
+                Files.createDirectories(parent);
+            }
+
+            try (ObjectOutputStream oos = new ObjectOutputStream(
+                    new BufferedOutputStream(Files.newOutputStream(userStatusPath)))) {
+                oos.writeObject(userStatuses);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("파일 저장 중 오류가 발생했습니다.", e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void loadFromFile() {
+        if (!Files.exists(userStatusPath)) {
+            return;
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(
+                new BufferedInputStream(Files.newInputStream(userStatusPath)))) {
+
+            List<UserStatus> loadedUserStatuses = (List<UserStatus>) ois.readObject();
+            userStatuses.clear();
+            userStatuses.addAll(loadedUserStatuses);
+
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException("파일을 불러오는 중 오류가 발생했습니다.", e);
+        }
+    }
+}
