b98a3ed0417704ce045e7084b7df725195528028
s-j012 <ddrwsy3800@naver.com>
2026-06-11 10:29:06 +0900

    스프린트 미션2 1차 추가/수정(심화) 서비스 간 의존성 주입 - 도메인 모델 간 관계 검증로직 추가 및 테스트

--- src/main/java/com/sprint/mission/JavaApplication.java
+++ src/main/java/com/sprint/mission/JavaApplication.java
@@ -22,7 +22,7 @@ public class JavaApplication {
             String username = scanner.nextLine();
             System.out.print("이메일 입력(예시: woody@codeit.com): ");
             String email = scanner.nextLine();
-            System.out.print("비밀번호 입력(에시: woody1234): ");
+            System.out.print("비밀번호 입력(예시: woody1234): ");
             String password = scanner.nextLine();
 
             //생성
@@ -95,7 +95,6 @@ public class JavaApplication {
         }
         static void messageCRUDTest(MessageService messageService,Channel channel, User user, Scanner scanner) {
             //생성
-
             System.out.print("메시지 입력: ");
             String content = scanner.nextLine();
             Message message = messageService.create(content, channel.getId(), user.getId());
@@ -127,12 +126,50 @@ public class JavaApplication {
             }
         }
 
+        static User setupUser(UserService userService) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("사용자명 입력(예시: woody): ");
+            String username = scanner.nextLine();
+
+            System.out.print("이메일 입력(예시: woody@codeit.com): ");
+            String email = scanner.nextLine();
+
+            System.out.print("비밀번호 입력(예시: woody1234): ");
+            String password = scanner.nextLine();
+
+            User user = userService.create(username, email, password);
+            return user;
+        }
+
+        static Channel setupChannel(ChannelService channelService) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("채널 타입(PUBLIC/PRIVATE): ");
+            ChannelType type = ChannelType.valueOf(
+                    scanner.nextLine().toUpperCase());
+
+            System.out.print("채널명 입력(예시: 공지): ");
+            String name = scanner.nextLine();
+
+            System.out.print("채널 설명 입력(예시: 공지채널입니다.): ");
+            String description = scanner.nextLine();
+            Channel channel = channelService.create(type, name, description);
+            return channel;
+        }
+
+        static void messageCreateTest(MessageService messageService, Channel channel, User author) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("메시지 입력: ");
+            String content = scanner.nextLine();
+            Message message = messageService.create(content, channel.getId(), author.getId());
+            System.out.println("메시지 생성: " + message.getId());
+        }
+
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in); //입력을 받기 위한 Scanner 객체 생성
             //서비스 초기화
             UserService userService = new JCFUserService();
             ChannelService channelService = new JCFChannelService();
-            MessageService messageService = new JCFMessageService();
+            MessageService messageService = new JCFMessageService(channelService, userService);
 
             //테스트
             userCRUDTest(userService, scanner);
@@ -140,7 +177,7 @@ public class JavaApplication {
 
             User user =
                     userService.create(
-                            "testUser",
+                            "test",
                             "test@test.com",
                             "1234");
 
@@ -148,15 +185,167 @@ public class JavaApplication {
                     channelService.create(
                             ChannelType.PUBLIC,
                             "general",
-                            "테스트 채널");
+                            "테스트");
+            messageCRUDTest(messageService, channel, user, scanner);
 
-            messageCRUDTest(
-                    messageService,
-                    channel,
-                    user,
-                    scanner);
+            /*//셋업
+            User usersetup = setupUser(userService);
+            Channel channelsetup = setupChannel(channelService);
 
+            //테스트
+            messageCreateTest(messageService, channelsetup, usersetup);*/
         }
 
+    /*
+     static User setupUser(UserService userService) {
+
+        User user = userService.create("woody","woody@codeit.com","woody1234");
+        return user;
+    }
+
+    static Channel setupChannel(ChannelService channelService) {
+        Channel channel = channelService.create(ChannelType.PUBLIC, "공지","공지채널입니다.");
+        return channel;
+    }
+
+    static void MessageCreateTest(MessageService messageService, Channel channel, User userId) {
+        Message message = messageService.create("안녕하세요.",channel.getId(), userId.getId());
+        System.out.println("메시지 생성: " + message.getId());
+    }
+
+    public static void main(String[] args) {
+        // 1. 각 도메인별 서비스 구현체 준비
+        UserRepository userRepository = new FileUserRepository();
+        ChannelRepository channelRepository = new FileChannelRepository();
+        MessageRepository messageRepository = new FileMessageRepository();
+
+        UserService userService = new BasicUserService(UserRepository);
+        ChannelService channelService = new BasicChannelService(channelRepository);
+        MessageService messageService = new BasicMessageService(messageRepository, channelRepository, userRepository);
+
+        Scanner scanner = new Scanner(System.in); //입력을 받기 위한 Scanner 객체 생성
+
+        User user = setupUser(userService);
+        Channel channel = setupChannel(channelService);
+
+        MessageCreateTest(messageService, channel, user);
+
+        *
+        //User 테스트
+
+        //등록(Create)
+        System.out.print("등록할 사용자 이름을 입력하세요: "); //사용자에게 이름 입력을 요청하는 메시지 출력
+        String userName = scanner.nextLine(); //사용자가 입력한 이름을 읽어서 userName 변수에 저장
+        User user = new User(userName); //입력받은 이름으로 User 객체 생성
+        userService.create(user); //생성한 User 객체를 userService의 create 메소드로 등록
+
+        //단건조회(Read)
+        User foundUser = userService.read(user.getId()); //id로 한 명 찾기
+        System.out.println("조회한 사용자 이름: " + foundUser.getName()); //결과 출력
+
+        //전체조회(ReadAll)
+        List<User> users = userService.readAll(); //전체 리스트 가져오기
+        System.out.println("전체 사용자 수: " + users.size());
+
+        //수정(Update)
+        System.out.print("수정할 사용자 이름을 적어주세요: "); //변경할 사용자 이름 입력
+        String newUserName = scanner.nextLine();
+        user.updateName(newUserName); //User 객체 내부이름변경
+        userService.update(user); //변경 정보를 서비스에 반영
+
+        //수정된 데이터 조회
+        User updatedUser = userService.read(user.getId());// 수정된 사용자 정보를 다시 조회하여 변경 사항이 정상적으로 저장되었는지 확인
+        System.out.println("수정 후 사용자 이름: " + updatedUser.getName());
+
+        //삭제(Delete)
+        userService.delete(user.getId()); //사용자 삭제
+
+        //삭제 후 조회(존재하는지 확인)
+        User deletedUser = userService.read(user.getId());
+        if (deletedUser == null) { // 삭제 후 다시 조회하여 실제로 삭제되었는지 확인
+            System.out.println("사용자가 성공적으로 삭제되었습니다.");
+        }
+
+
+        // CHannel테스트
+        //등록
+        System.out.print("등록할 채널명을 입력하세요: ");
+        String channelName = scanner.nextLine();
+        Channel channel = new Channel(channelName);
+        channelService.create(channel);
+
+        //단건조회
+        Channel foundChannel = channelService.read(channel.getId());
+        System.out.println("조회한 채널명: " + foundChannel.getName());
+
+        //전체조회
+        List<Channel> channels = channelService.readAll();
+        System.out.println("전체 채널 수: " + channels.size());
+
+        //수정
+        System.out.print("수정할 채널명을 적어주세요: ");
+        String newChannelName = scanner.nextLine();
+        channel.updateName(newChannelName);
+        channelService.update(channel);
+
+        //수정된 데이터 조회
+        Channel updatedchannel = channelService.read(channel.getId());
+        System.out.println("수정 후 채널 이름: " + updatedchannel.getName());
+
+        //삭제
+        channelService.delete(channel.getId());
+
+        //삭제 후 조회
+        Channel deletedChannel = channelService.read(channel.getId());
+        if (deletedChannel == null) {
+            System.out.println("채널이 성공적으로 삭제되었습니다.");
+        }
+
+
+        //Message테스트
+        //등록
+        System.out.print("메세지 보낼 사용자이름을 입력해주세요: ");
+        String messageUserName = scanner.nextLine();
+        User messageUser = new User(messageUserName);
+        userService.create(messageUser);
+
+        System.out.print("메세지 보낼 채널명을 입력해주세요: ");
+        String messageChannelName = scanner.nextLine();
+        Channel messageChannel = new Channel(messageChannelName);
+        channelService.create(messageChannel);
+
+        System.out.print("등록할 메시지 내용을 입력해주세요: ");
+        String messageContent = scanner.nextLine();
+        Message message = new Message(messageContent, messageUser.getId(), messageChannel.getId());
+        messageService.create(message);
+
+        //단건조회
+        Message foundMessage = messageService.read(message.getId());
+        System.out.println("조회한 메세지 이름: " + foundMessage.getContent());
+
+        //전체조회
+        List<Message> messages = messageService.readAll();
+        System.out.println("전체 메세지 수: " + messages.size());
+
+        //수정
+        System.out.print("수정할 메세지 내용을 적어주세요: ");
+        String newMessageContent = scanner.nextLine();
+        message.updateContent(newMessageContent);
+        messageService.update(message);
+
+        //수정된 데이터 조회
+        Message updatedMessage = messageService.read(message.getId());
+        System.out.println("수정 후 메세지 이름: " + updatedMessage.getContent());
+
+        //삭제
+        messageService.delete(message.getId());
+
+        //삭제 후 조회
+        Message deletedMessage = messageService.read(message.getId());
+        if (deletedMessage == null) {
+            System.out.println("메세지가 성공적으로 삭제되었습니다.");
+        } *
+
+    }*/
 
 }
--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
@@ -1,7 +1,9 @@
 package com.sprint.mission.discodeit.service.jcf;
 
 import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
 
 import java.util.*;
 
@@ -9,11 +11,23 @@ import java.util.*;
 public class JCFMessageService implements MessageService {
 
     private final Map<UUID, Message> data; //메시지 데이터를 저장하는 메모리 저장소
+    private final ChannelService channelService; //메시지가 작성될 채널을 조회하기 위한 서비스
+    private final UserService userService; //메시지 작성자를 조회하기 위한 서비스
 
-    public JCFMessageService() {this.data = new HashMap<>();}
+    public JCFMessageService(ChannelService channelService, UserService userService) {
+        this.data = new HashMap<>();
+        this.channelService = channelService;
+        this.userService = userService;
+    }
 
     @Override //메시지 생성
     public Message create(String content, UUID channelId, UUID authorId) {
+        try { // 1.채널 존재 여부 확인, 2.사용자 존재여부확인, 둘중 하나라도 미존재 시 NoSuchElementException 발생
+            channelService.find(channelId);
+            userService.find(authorId);
+        } catch (NoSuchElementException e) {
+            throw e;
+        }
         Message message = new Message(content, channelId, authorId);
         this.data.put(message.getId(), message);
         return message;
