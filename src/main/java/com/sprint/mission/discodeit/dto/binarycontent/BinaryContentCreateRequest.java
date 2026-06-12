4f1c20b9bc6c4722eaf74ed9fc27eb22fe7bd46c
seok-dev <kseok.dev@gmail.com>
2026-06-13 03:08:05 +0900

    DTO 추가 생성 -MessageResponse -UserStatusResponse -ReadStatusResponse -BinaryContentResponse

--- src/main/java/com/sprint/mission/discodeit/dto/binarycontent/BinaryContentCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/binarycontent/BinaryContentCreateRequest.java
@@ -6,10 +6,9 @@ import java.util.Arrays;
 import java.util.UUID;
 
 public record BinaryContentCreateRequest(
-        UUID userId,
         String fileName,
         long fileSize,
-        String contentType,
+        ContentType contentType,
         byte[] bytes
 )
 {
@@ -19,13 +18,6 @@ public record BinaryContentCreateRequest(
         //파일 크기 검증
         if (fileSize > MAX_FILE_SIZE) {
             throw new IllegalArgumentException("파일 크기는 5MB 이하여야 합니다.");
-        }
-
-        //파일 타입검증
-        boolean isAllowed = Arrays.stream(ContentType.values())
-                .anyMatch(ct -> ct.getValue().equals(contentType));
-        if (!isAllowed) {
-            throw new IllegalArgumentException("지원하지 않는 파일 형식입니다.");
         }
     }
 
---
+++ src/main/java/com/sprint/mission/discodeit/dto/message/MessageResponse.java
@@ -0,0 +1,18 @@
+package com.sprint.mission.discodeit.dto.message;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+public record MessageResponse(
+        UUID id,
+        Instant createdAt,
+        Instant updatedAt,
+        String content,
+        UUID authorId,
+        List<UUID> attachmentIds
+
+)
+{
+
+}
