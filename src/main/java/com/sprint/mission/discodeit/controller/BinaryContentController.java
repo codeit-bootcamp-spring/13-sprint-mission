8308f5f2f3e996dc7e4bb02669dce398294e7acb
hisjeans <his7jeans@gmail.com>
2026-06-23 13:21:13 +0900

    @Controller 를 이용해 API 구현 - 'PrivateChannelCreateRequest'에 'name', 'description' 필드 삭제 - @RestController를 @Controller로 변경, @ResponseBody 추가

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -3,28 +3,33 @@ package com.sprint.mission.discodeit.controller;
 import com.sprint.mission.discodeit.dto.binarycontent.BinaryContentResponse;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
-@RestController
+@Controller
 @RequestMapping("/api/binary-contents")
 @RequiredArgsConstructor
+@ResponseBody
 public class BinaryContentController {
 
-    private final BinaryContentService binaryContentService;
+  private final BinaryContentService binaryContentService;
 
-    // 바이너리 파일을 1개 또는 여러 개 조회
-    @RequestMapping(value = "/{contentId}", method = RequestMethod.GET)
-    public ResponseEntity<BinaryContentResponse> find(@PathVariable UUID contentId) {
-        BinaryContentResponse foundContent = binaryContentService.find(contentId);
-        return ResponseEntity.ok().body(foundContent);
-    }
-    @RequestMapping(value = "/{contentIds}", method = RequestMethod.GET)
-    public ResponseEntity<List<BinaryContentResponse>> findAllByIdIn(@PathVariable List<UUID> contentIds) {
-        List<BinaryContentResponse> foundAllByIdIn = binaryContentService.findAllByIdIn(contentIds);
-        return ResponseEntity.ok().body(foundAllByIdIn);
-    }
+  // 바이너리 파일을 1개 또는 여러 개 조회
+  @RequestMapping(path = "/{contentId}", method = RequestMethod.GET)
+  public ResponseEntity<BinaryContentResponse> find(@PathVariable UUID contentId) {
+    BinaryContentResponse foundContent = binaryContentService.find(contentId);
+    return ResponseEntity.status(HttpStatus.OK).body(foundContent);
+  }
+
+  @RequestMapping(method = RequestMethod.GET)
+  public ResponseEntity<List<BinaryContentResponse>> findAllByIdIn(
+      @RequestParam(required = false) List<UUID> contentIds) {
+    List<BinaryContentResponse> foundAllByIdIn = binaryContentService.findAllByIdIn(contentIds);
+    return ResponseEntity.status(HttpStatus.OK).body(foundAllByIdIn);
+  }
 }
