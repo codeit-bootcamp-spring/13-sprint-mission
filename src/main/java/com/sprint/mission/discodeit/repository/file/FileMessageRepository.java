f13e1346ccbdc39dfb87361fd8e4ce01db82e890
seok-dev <kseok.dev@gmail.com>
2026-06-28 17:21:59 +0900

    File Repository EOFException 처리 추가

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
@@ -47,7 +47,11 @@ public class FileMessageRepository implements MessageRepository {
         }
         try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath.toFile()))){
             return (Map<UUID, Message>) ois.readObject();
-        }catch (IOException | ClassNotFoundException e){
+        }
+        catch (EOFException e){
+            return new HashMap<>();
+        }
+        catch (IOException | ClassNotFoundException e){
             System.out.println("에러 타입: " + e.getClass().getName());
             System.out.println("에러 메시지: " + e.getMessage());
             throw new RuntimeException("파일 불러오기 실패");
