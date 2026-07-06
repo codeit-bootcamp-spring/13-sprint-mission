51618628200501120a70cd47d8bb4a22e10dd9ae
jangjunseo518-collab <wkdwnstj00@naver.com>
2026-07-06 10:08:40 +0900

    Repository JpaRepository로 교체 및 File/JCF Repository 삭제

--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -3,18 +3,11 @@ package com.sprint.mission.discodeit.repository;
 import com.sprint.mission.discodeit.entity.Message;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MessageRepository {
+public interface MessageRepository extends JpaRepository<Message, UUID> {
 
-    Message save(Message message);
-
-   Optional<Message> findById(UUID id);
-
-    List<Message> findAll();
-
-    boolean existsById(UUID id);
-
-    void deleteById(UUID id);
+  // 변경 후 (channel 객체의 id로 조회/Channel_Id → channel 필드의 id를 참조)
+  List<Message> findAllByChannel_Id(UUID channelId);
 }
