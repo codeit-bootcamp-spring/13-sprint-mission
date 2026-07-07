7132ae531b18ec3a8dc2df3ec564b39efbb4e66c
marcel615 <alswo000615@gmail.com>
2026-07-07 14:07:03 +0900

    feat: 스프린트 미션6 (11: N+1 문제)
    - application.yaml에 실행되는 SQL을 콘솔에 보여주는 기능 추가
    - UserRepository에서 findAll() 메서드에서 status, profile을 함께 조회하도록 EntityGraph 적용
    - MessageRepository에서 findAllByChannelId() 메서드에서 channel, author, attachments를 함께 조회하도록 EntityGraph 적용
    - ReadStatusRepository에서 findAllByUserId() 메서드에서 user, channel를 함께 조회하도록 EntityGraph 적용
    - ReadStatusRepository에서 findAllByChannelId() 메서드에서 user를 함께 조회하도록 EntityGraph 적용

--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -3,6 +3,7 @@ package com.sprint.mission.discodeit.repository;
 import com.sprint.mission.discodeit.entity.Message;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -18,7 +19,9 @@ public interface MessageRepository extends JpaRepository<Message, UUID> {
 //    void deleteMessagesByChannelId(UUID channelId);
 //    void deleteMessageById(UUID id);
 
+    @EntityGraph(attributePaths = {"channel", "author", "attachments"})
     Slice<Message> findAllByChannelId(UUID channelId, Pageable pageable);
+
     List<Message> findAllByChannelId(UUID channelId);
     List<Message> findAllByAuthorId(UUID authorId);
     void deleteAllByChannelId(UUID channelId);
--- src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -21,6 +22,10 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+
+    @Override
+    @EntityGraph(attributePaths = {"profile", "status"})
+    List<User> findAll();
     Optional<User> findByUsernameAndPassword(String username, String password);
 
 
