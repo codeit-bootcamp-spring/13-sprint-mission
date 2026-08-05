449088e2cdd471793a3ec5df127a37c1e8ccd9e5
woong <skarn506@gmail.com>
2026-08-05 13:33:51 +0900

    feat: S3 기반 파일 저장소 구현

---
+++ src/main/java/com/sprint/mission/discodeit/storage/s3/S3Properties.java
@@ -0,0 +1,18 @@
+package com.sprint.mission.discodeit.storage.s3;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "discodeit.storage.se")
+public class S3Properties {
+
+  private String region = "ap-northeast-2";
+
+  private String bucket;
+
+  private long presignedUrlExpiration = 600;
+
+}
