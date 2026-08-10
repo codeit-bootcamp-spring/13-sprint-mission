847b72878982513dc80e255acf1968625f01999c
hajung <bover80@naver.com>
2026-08-10 17:02:51 +0900

    애플리케이션 컨테이너화 - Dockerfile 작성 - 이미지 빌드 및 실행 테스트 - Docker Compose 구성

--- src/main/java/com/sprint/mission/discodeit/conpig/SwaggerConfig.java
+++ src/main/java/com/sprint/mission/discodeit/conpig/SwaggerConfig.java
@@ -3,17 +3,22 @@ package com.sprint.mission.discodeit.conpig;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${swagger.server-url}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI customOpenApi(){
         List<Server> servers = List.of(
-                new Server().url("http://localhost:8080").description("로컬 개발 서버")
+                new Server().url(serverUrl).description("로컬 개발 서버")
         );
 
         return new OpenAPI()
