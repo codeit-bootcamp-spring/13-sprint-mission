747a91de4f1f39728bc330687aff507b6a63b6b2
marcel615 <alswo000615@gmail.com>
2026-09-12 17:09:55 +0900

    feat: 스프린트 미션9 (05: 인증 - 세션을 활용한 현재 사용자 정보 조회)
    - AuthController에 GET /api/auth/me api 추가

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,10 +1,13 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.response.UserDto;
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +25,12 @@ public class AuthController {
         log.debug("CSRF 토큰 요청: {}", tokenValue);
 
         return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me(@AuthenticationPrincipal DiscodeitUserDetails userDetails) {
+        return ResponseEntity.ok(userDetails.getUserDto());
+
     }
 
 }
