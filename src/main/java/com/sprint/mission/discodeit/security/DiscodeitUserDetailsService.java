9a938f6b489a7c1e51d3984b0aa53aa278c2a2f1
latte798 <latte0798@gmail.com>
2026-09-20 10:13:07 +0000

    add: 프런트 리소스 업데이트

--- src/main/java/com/sprint/mission/discodeit/security/DiscodeitUserDetailsService.java
+++ src/main/java/com/sprint/mission/discodeit/security/DiscodeitUserDetailsService.java
@@ -3,10 +3,13 @@ package com.sprint.mission.discodeit.security;
 
 import com.sprint.mission.discodeit.dto.projection.UserProjection;
 import com.sprint.mission.discodeit.dto.response.UserDto;
+import com.sprint.mission.discodeit.exception.DiscodeitException;
+import com.sprint.mission.discodeit.exception.ExceptionCode;
 import com.sprint.mission.discodeit.mapper.MapStructMapper;
 import com.sprint.mission.discodeit.mapper.MapperMethod;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class DiscodeitUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
@@ -31,7 +35,10 @@ public class DiscodeitUserDetailsService implements UserDetailsService {
         // username 을 통해 username (email) 을 가져온다.
 
         UserProjection projection = userRepository.getUserFromUsername(username)
-                .orElseThrow(RuntimeException::new);
+                .orElseThrow(() -> {
+                    log.warn("UserDetails - 유저 조회 오류");
+                    return new DiscodeitException(ExceptionCode.AUTH_FAILURE,"인증 오류");
+                });
 
         // 본인 정보 반환이라 online true 반환.
         UserDto dto = mapper.toDto(projection,mapper.toDto(projection,mapperMethod),true);
